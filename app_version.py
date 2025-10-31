af3211aa4b45b433498a36e7ec0d81cc2e510eb2
alexa <alexa@users.noreply.github.com>
2025-10-30 21:54:27 -0400

    Release: bump to 0.2.0

--- app_version.py
+++ app_version.py
@@ -1,5 +1,5 @@
-APP_VERSION = "0.1.0"
-APP_VERSION = "0.1.1"
+APP_VERSION = "0.2.0"
 UPDATE_CHANNEL = "stable"  # 'stable' | 'beta' | 'alpha'
 GITHUB_OWNER = "xtreme00zzz"
 GITHUB_REPO = "car_inspection"
+
